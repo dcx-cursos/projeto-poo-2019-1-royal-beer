7f9aef2507de888b5b7459be25df65faa649eb85
MatMr7 <matheus.sousa@dce.ufpb.br>
2019-08-28 23:28:35 -0300

    Story 1 refeito com padrao de projeto Facade

--- BancoImobiliario_POO/src/principal/JogoFacade.java
+++ BancoImobiliario_POO/src/principal/JogoFacade.java
@@ -12,25 +12,24 @@ public class JogoFacade  {
 
 	private Tabuleiro tabuleiro = new Tabuleiro();
 	
-	
 	private ArrayList<Jogador> jogadores = new ArrayList<>();
 
 	private ArrayList<String>  coresDisponiveis = carregaCores();
+	
+	private ArrayList<String> comandosDisponiveis = carregaComandos();
+	
+	private int ponteiro = 0;
 
 	
 	public void cadastraJogador(String nome , String cor) throws CorIndisponivelException{
 		if(this.jogadorComCor(cor) == -1) {
-			if(this.verificaCorNaLista(cor) == -1){
+			if(this.verificaCorNaLista(cor) != -1){
 				Jogador jog = new Jogador(nome,cor);
 				this.removeCorDaLista(cor);
 				jogadores.add(jog);
 			}else throw new CorIndisponivelException("A cor escolhida não está dentre as opções");
 		}else throw new CorIndisponivelException("Cor ja escolhida");
-		
-	
-}
-	
-	
+	}
 	
 	private int jogadorComCor(String cor) {
 		for(int k = 0; k<this.jogadores.size();k++) {
@@ -41,16 +40,14 @@ public class JogoFacade  {
 		return -1;
 	}
 	
-	private int verificaCorNaLista(String item) {
-		int cont = 0;
+	public int verificaCorNaLista(String item) {
 		for(int k =0;k<coresDisponiveis.size();k++) {
-			if(item.equals(this.coresDisponiveis.get(k))) return k;
-			cont ++;
+			if(item.toUpperCase().equals(this.coresDisponiveis.get(k))) return k;
+			
 		}
 		return -1;
 	}		
 
-	
 	private void removeCorDaLista(String cor) {
 		for(int k = 0 ; k<this.coresDisponiveis.size();k++) {
 			if(this.coresDisponiveis.get(k).equals(cor.toUpperCase())) {
@@ -77,7 +74,7 @@ public class JogoFacade  {
 		return this.jogadores;
 	}
 	
-	public ArrayList<String> carregaCores() {
+	private ArrayList<String> carregaCores() {
 		ArrayList<String> temp = new ArrayList<>();
 		temp.add("PRETO");
 		temp.add("BRANCO");
@@ -90,6 +87,40 @@ public class JogoFacade  {
 		
 		return temp;
 	}
+	
+	public boolean verificaNumeroDeJogadores(int numeroDeJogadores) {
+		if(numeroDeJogadores >= 2 && numeroDeJogadores <=8) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Jogador getProxJogador() {
+		this.ponteiro+= 1;
+		if(this.ponteiro == this.jogadores.size()) {
+			this.ponteiro = 0;
+		}
+			return this.jogadores.get(this.ponteiro);
+		
+		
+	}
+	
+	private ArrayList<String> carregaComandos(){
+		ArrayList<String> temp = new ArrayList<String>();
+		temp.add("Jogar");
+		temp.add("Sair");
+		return temp;
+	}
+	
+	public String getStringDeComandos() {
+		String temp = "";
+		for(String a : this.comandosDisponiveis) {
+			temp += "["+ a +"]";
+		}
+		return temp;
+		
+	}
+	
 	
 	
 	
--- BancoImobiliario_POO/src/principal/Main.java
+++ BancoImobiliario_POO/src/principal/Main.java
@@ -1,19 +1,82 @@
 package principal;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import busca.Lista;
-import tabuleiro.Tabuleiro;
+import java.util.Scanner;
+
+import exceptions.CorIndisponivelException;
+
+
 
 public class Main {
 	public static void main(String args[]) {
 
 		JogoFacade facade = new JogoFacade();
+		Scanner sc = new Scanner(System.in);
+		int numJogadores = 0;
+		String corNovoJogador = null;
+		
+		//Inicio do laço para seleção de quantidade de jogadores
+		while(true) {
+		System.out.println("Digite o numero de jogadores");
+		try {
+			numJogadores= Integer.parseInt(sc.nextLine());
+			if(facade.verificaNumeroDeJogadores(numJogadores)) {
+				break;
+			}else {
+				System.out.println("Entrada inválida");
+				continue;
+			}
+			
+		}catch(NullPointerException|NumberFormatException e) {
+			System.out.println("Entrada inválida");
+			continue;
+		}
+		}//Fim do laço da seleção da quantidade de jogadores
+		
+		
+		//inicio do laço de cadastro de jogadores
+		for(int k =0;k<numJogadores;k++) {
+			System.out.println("Digite o nome do jogador "+(k+1)+": ");
+			String nomeNovoJogador = sc.nextLine();
+
+			//Inicio do laço para seleção de cor
+			while(true) {
+				System.out.println("Digite a cor do jogador "+(k+1)+ " dentre as opções abaixo");
+				System.out.println(facade.getStringDeCores());
+				corNovoJogador = sc.nextLine();
+				if(facade.verificaCorNaLista(corNovoJogador) != -1) {
+					break;
+				}else {
+					System.out.println("Cor inválida");	
+				}	
+			}
+			//Fim do laço para seleção de cor
+			try {
+				facade.cadastraJogador(nomeNovoJogador, corNovoJogador);
+			}catch(CorIndisponivelException e) {
+				k--;
+				System.out.println(e.toString());
+			}
+			}
+		//Fim do laço de cadastro de jogadores
+		
+		
+		//Começo do Jogo
+		
+		System.out.println("O Banco Imobiliario vai começar. Aproveite!!");
+		
+		//Inicio do laço do jogo
+		while(facade.getJogadores().size() >= 1) {
+			Jogador jogadorDaVez = facade.getProxJogador();
+			System.out.println("A jogada de "+ jogadorDaVez.getNome()+" ("+jogadorDaVez.getCor()+") "
+					+"começou");
+			System.out.println("Comandos disponiveis : "+ facade.getStringDeComandos()
+			+"\nEntre com um comando");
+			String comandoSelecionado = sc.nextLine();
+		}
+		
 		
 		
 	}//fim main
 
-}
+}	
